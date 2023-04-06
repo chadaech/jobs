0c3de3ecb86fa5a8a586d3780b054c0a8ac4d019
chadaech <chadaech@gmail.com>
2023-04-06 16:25:00 -0400

    more java

---
+++ Java Practice/JavaPractice.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+//This demo is for java practice
+class RandomNumber{
+	public static void main( String args[] ) {
+	// Instance of random class
+	Random rand = new Random();
+	// Setting the upper bound to generate the
+	// random numbers in specific range
+	int upperbound = 55;
+	// Generating random values from 0 - 55
+	// using nextInt()
+	int int_random = rand.nextInt(upperbound);
+	// Generating random using nextDouble
+	// in 0.0 and 1.0 range
+	double double_random = rand.nextDouble();
+	// Generating random using nextDouble
+	// in 0.0 and 1.0 range
+	float float_random = rand.nextFloat();
+
+	// Printing the generated random numbers out to text
+	System.out.println("Random interger value from 0 to " + (upperbound-1) + " : " + int_random);
+	System.out.println("Random float value between 0.0 and 1.0 : " + float_random);
+	System.out.println("Random double value between 0.0 and 1.0 : " + double_random);
+	}
+}
